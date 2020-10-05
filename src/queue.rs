25d3a13f47c42d5ce3e8c942d721efde2a8c6895
robert_blackwell <rob@whiteacorn.com>
2020-10-05 07:32:20 -0700

    queue coded not run

--- src/queue.rs
+++ src/queue.rs
@@ -1,6 +1,8 @@
 /// All of this is now in another module now
 use std::sync::{Arc, Condvar, Mutex};
 use std::collections::LinkedList;
+use std::convert::TryInto;
+use std::cell::RefCell;
 
 const NBR_WORKERS: usize = 5;
 const QUEUE_MAX: usize = 5;
@@ -10,7 +12,7 @@ pub struct Queue {
     wrtr_cvar: Condvar,
     mutex: Mutex<i32>,
     capacity: usize,
-    list: LinkedList<i32>,
+    list: RefCell::<LinkedList<i32>>,
 }
 
 impl Queue {
@@ -20,29 +22,42 @@ impl Queue {
             wrtr_cvar: Condvar::new(),
             mutex: Mutex::new(0),
             capacity: capacity,
-            list: LinkedList::<i32>::new()
+            list: RefCell::new(LinkedList::<i32>::new())
         }
 
     }
     pub fn add(self: &Queue, sock: i32) {
+        let q = self;
+        let max:i32  = self.capacity.try_into().unwrap();
+        let guard = self.mutex.lock().unwrap();
+        let mut guard = self.wrtr_cvar.wait_while(guard, |n| *n > max).unwrap();
 
-    }
-    pub fn remove(qq: &Queue) -> i32 {
-        let mut  result: i32 = -1;
-        // let q = qq;
-        // let guard = q.mutex.lock().unwrap();
-        // let mut guard = q.rdr_cvar.wait_while(guard, |n| *n <= 0).unwrap();
-
-        // println!("{} Reader {} n_resource = {}\n", result, result, *guard);
+        println!("Queue::add  *guard {} \n", *guard);
         
-        // *guard -= 1;
+        self.list.borrow_mut().push_back(sock);
+        *guard += 1;
 
-        // if *guard > 0 {
-        //     q.rdr_cvar.notify_one();
-        // }
-        // if *guard < 10 {
-        //     q.wrtr_cvar.notify_one();
-        // }
+        if *guard > 0 {
+            self.rdr_cvar.notify_one();
+        }
+        if *guard < max {
+            q.wrtr_cvar.notify_one();
+        }
+    }
+    pub fn remove(self: &Queue) -> i32 {
+        let guard = self.mutex.lock().unwrap();
+        let mut guard = self.rdr_cvar.wait_while(guard, |n| *n <= 0).unwrap();
+
+        println!("Queue::remove guard  = {}\n", *guard);
+        
+        *guard -= 1;
+        let result = self.list.borrow_mut().pop_front().unwrap();
+        if *guard > 0 {
+            self.rdr_cvar.notify_one();
+        }
+        if *guard < 10 {
+            self.wrtr_cvar.notify_one();
+        }
         return result;
     }
 }
--- src/reglib/mod.rs
+++
@@ -1,7 +0,0 @@
-/// When you load `module_abc` in main "mod module_abc;" cargo looks for a file module_abc.rs
-/// in the same folder than main.rs, if doesn't find one looks for a file module_abc/mod.rs
-/// (this one), so here we call the others
-
-pub mod server;
-pub mod worker;
-pub mod queue;
